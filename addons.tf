59f63f44a3c70a15f4d408dbe1209708b675dcc2
Brad Sickles <brad.sickles@gmail.com>
2026-02-17 10:07:30 -0500

    Added secrets and efs addons

---
+++ addons.tf
@@ -0,0 +1,19 @@
+# eks-pod-identity-agent
+# load balancer controller
+# karpenter or cluster-autoscaler
+
+resource "aws_eks_addon" "secrets" {
+  cluster_name                = aws_eks_cluster.this.name
+  addon_name                  = "aws-secrets-store-csi-driver-provider"
+  addon_version               = null
+  resolve_conflicts_on_update = "PRESERVE"
+  tags                        = local.tags
+}
+
+resource "aws_eks_addon" "efs" {
+  cluster_name                = aws_eks_cluster.this.name
+  addon_name                  = "aws-efs-csi-driver"
+  addon_version               = null
+  resolve_conflicts_on_update = "PRESERVE"
+  tags                        = local.tags
+}
